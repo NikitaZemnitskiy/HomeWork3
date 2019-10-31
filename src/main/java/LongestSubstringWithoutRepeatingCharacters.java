6b10355cccf1a466186d1f39498359bb2af3ff9b
NikitaZemnitskiy <nik.zemnitskiy@gmail.com>
2019-10-31 23:23:29 +0200

    save the current state

--- src/main/java/LongestSubstringWithoutRepeatingCharacters.java
+++ src/main/java/LongestSubstringWithoutRepeatingCharacters.java
@@ -5,37 +5,22 @@ import java.util.Set;
 
 public class LongestSubstringWithoutRepeatingCharacters {
     public static void main(String[] args) {
-        System.out.println(lengthOfLongestSubstring("aa"));
+        String s = "Коллективизация";
+       System.out.println(lengthOfLongestSubstring(s));
     }
-
 
 
     public static int lengthOfLongestSubstring(String s) {
-        String first = s.substring(0,1);
-        String[] res = s.split(first);
-        System.out.println(res);
-        return 1;
-    }
-    public static boolean isRepeatMass(char[]arr, int first,int last){
-        for (int i = first; i<last; i++){
-            for (int k = i+1; k<last; k++){
-                if(arr[i] == arr[k]){
-                    return true;
-                }
+        for (int i = 1; i<s.length();i++){
+            String currentString = s.substring(0,i);
+            if (currentString.indexOf(s.charAt(i)) != -1){
+                String prefix = s.substring(0,s.indexOf(s.charAt(i))+1);
+                String sufix = s.substring(s.indexOf(s.charAt(i))+1);
+                int sufixLength = lengthOfLongestSubstring(sufix);
+                int MaxLength = sufixLength>prefix.length()?sufixLength:prefix.length();
+                return MaxLength>currentString.length()?MaxLength:currentString.length();
             }
         }
-       return false;
+        return s.length();
     }
-
-    public static boolean isRepeat (char[]arr, int first,int last){
-        Set<Character>set = new HashSet<Character>();
-        for (int i = first; i<last; i++){
-            if (set.contains(arr[i])){
-                return true;
-            }
-            set.add(arr[i]);
-        }
-
-        return true;
-        }
 }
